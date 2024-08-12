bb719df2b24d8ba30e2629a8915a44ad4ddcf9c6
Gleb Klyga <klyga90@bk.ru>
2024-08-13 03:34:40 +0500

    Add tasks 2.3 and 2.4

--- HumanApps_Tech/HumanApps_Tech/MainViewController.swift
+++ HumanApps_Tech/HumanApps_Tech/MainViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Combine
 
 class MainViewController: UIViewController {
 
@@ -20,11 +21,20 @@ class MainViewController: UIViewController {
         return imageView
     }()
     
+    private let viewModel = MainViewModel()
+    private var cancellables: Set<AnyCancellable> = []
+    
+    // Gesture recognizers
+    private var panGesture: UIPanGestureRecognizer!
+    private var pinchGesture: UIPinchGestureRecognizer!
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        bindViewModel()
+        setupGestures()
     }
     
     // MARK: - UI Setup
@@ -65,6 +75,40 @@ class MainViewController: UIViewController {
         ])
     }
     
+    private func bindViewModel() {
+        viewModel.$currentPhoto
+            .receive(on: RunLoop.main)
+            .sink { [weak self] image in
+                guard let self = self, let image = image else { return }
+                self.updateImageView(with: image)
+            }
+            .store(in: &cancellables)
+    }
+    
+    private func updateImageView(with image: UIImage) {
+        imageView.image = image
+        
+        let imageAspectRatio = image.size.width / image.size.height
+        
+        NSLayoutConstraint.deactivate(imageView.constraints)
+        
+        NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            imageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
+            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 1 / imageAspectRatio)
+        ])
+    }
+    
+    private func setupGestures() {
+        panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(panGesture)
+        
+        pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(handlePinch(_:)))
+        imageView.addGestureRecognizer(pinchGesture)
+    }
+    
     // MARK: - Actions
     
     @objc private func openPhotoGallery() {
@@ -75,6 +119,21 @@ class MainViewController: UIViewController {
         
         present(imagePickerController, animated: true, completion: nil)
     }
+    
+    @objc private func handlePan(_ recognizer: UIPanGestureRecognizer) {
+        guard recognizer.state == .changed || recognizer.state == .ended else { return }
+        
+        let translation = recognizer.translation(in: view)
+        recognizer.setTranslation(.zero, in: view)
+        imageView.center = CGPoint(x: imageView.center.x + translation.x, y: imageView.center.y + translation.y)
+    }
+    
+    @objc private func handlePinch(_ recognizer: UIPinchGestureRecognizer) {
+        guard recognizer.state == .changed || recognizer.state == .ended else { return }
+  
+        imageView.transform = imageView.transform.scaledBy(x: recognizer.scale, y: recognizer.scale)
+        recognizer.scale = 1
+    }
 }
 
 // MARK: - UIImagePickerControllerDelegate & UINavigationControllerDelegate
@@ -83,19 +142,8 @@ extension MainViewController: UIImagePickerControllerDelegate, UINavigationContr
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let selectedImage = info[.originalImage] as? UIImage {
-            imageView.image = selectedImage
-            
-            let imageAspectRatio = selectedImage.size.width / selectedImage.size.height
-            
-            NSLayoutConstraint.deactivate(imageView.constraints)
-            NSLayoutConstraint.activate([
-                imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-                imageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
-                imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 1 / imageAspectRatio)
-            ])
+            viewModel.addPhoto(selectedImage)
         }
-        
         picker.dismiss(animated: true, completion: nil)
     }
     
